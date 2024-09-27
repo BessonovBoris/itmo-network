d1d0313275c08e9f8684fd50702b25c0726b19db
Boris <borisimo.work@gmail.com>
2024-09-27 13:01:23 +0300

    CRLF->LF

--- lab-1/task-4/8_data_collection.bash
+++ lab-1/task-4/8_data_collection.bash
@@ -2,7 +2,7 @@
 
 for i in {1..3}; do
     echo "Запуск $i:"
-    ./currenе_time_Receive_Transmit.bash &
+    ./6_current_time_Receive_Transmit.bash &
     sleep 10
     kill $!
     echo "Скрипт network_stats.sh завершен."
